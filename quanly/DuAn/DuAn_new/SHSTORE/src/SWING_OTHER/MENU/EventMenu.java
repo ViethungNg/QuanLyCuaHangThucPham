b425f026ced6505d1f163e7ee8458812f5f8d35e
Duong <Duong@gmail.com>
2024-06-11 15:22:21 +0700

    dd

--- quanly/DuAn/DuAn_new/SHSTORE/src/SWING_OTHER/MENU/EventMenu.java
+++ quanly/DuAn/DuAn_new/SHSTORE/src/SWING_OTHER/MENU/EventMenu.java
@@ -5,6 +5,6 @@ import java.awt.Component;
 public interface EventMenu {
 
     public void mainMenuSelected(MainForm mainForm, int index, MenuItem menuItem);
-
+//da
     public void subMenuSelected(MainForm mainForm, int index, int subMenuIndex, Component menuItem);
 }
